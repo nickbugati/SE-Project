d986cc89a03034d99c7ff67857af5941d5114c94
Nicholas D. Gongee <nicholasgongee@aol.com>
2021-03-18 13:38:05 -0400

    Remove files

---
+++ server/models/examples.server.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const Schema = new mongoose.Schema({
+    
+    email: String,
+    password: String,
+    Firstname: String,
+    Lastname: String
+
+});
+exports.User = mongoose.model('User', Schema)
