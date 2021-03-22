414736d2dd74f1db55167ad56142b762c877a885
Nicholas D. Gongee <nicholasgongee@aol.com>
2021-03-22 13:24:21 -0400

    Front End

---
+++ client/src/views/Admin.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import DropDown from './Dropdown'
+
+function Admin() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                </a>
+            </header>
+        </div>
+    );
+}
