aaa1830c0c5abd173fa4472ad82e1f75fa825333
Nic Morita <44232086+CrazedOwlBeast@users.noreply.github.com>
2021-04-21 16:50:24 -0400

    Update officerModel.js

--- server/officerModel.js
+++ server/officerModel.js
@@ -1,3 +1,6 @@
+// Officer model and officer router were built following this guide by Devistry:
+// https://www.youtube.com/watch?v=scYojqjnHzI&list=PLJM1tXwlGdaf57oUx0rIqSW668Rpo_7oU&index=1
+//
 const mongoose = require("mongoose");
 
 const officerSchema = new mongoose.Schema({
